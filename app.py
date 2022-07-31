b2b672a5445d32e7ec928a300f42d9eccf0ba55c
ruxiz2020 <zruxi@vmware.com>
2022-07-31 12:19:51 -0700

    update

---
+++ app.py
@@ -0,0 +1,26 @@
+#----------------------------------------------------------------------------#
+# Imports
+#----------------------------------------------------------------------------#
+from flask import Flask, render_template
+
+
+#from . import app, db
+#----------------------------------------------------------------------------#
+# App Config.
+#----------------------------------------------------------------------------#
+
+app = Flask(__name__)
+
+
+app.config['CKEDITOR_SERVE_LOCAL'] = True
+app.config['CKEDITOR_HEIGHT'] = 400
+
+
+@app.route('/')
+def index():
+    return render_template('pages/home.html')
+
+
+# Default port:
+if __name__ == '__main__':
+    app.run(port=5052, debug=True)
